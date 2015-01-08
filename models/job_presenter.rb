f22b8ebf5cd3e641ffd0a466dab6aaad7b08437f
Simon Strom <simon.strom@gmail.com>
2015-01-08 20:59:40 +0100

    refactor preseneter

--- models/job_presenter.rb
+++ models/job_presenter.rb
@@ -2,12 +2,16 @@ module JobPresenter
   def apply_link
     url = "mailto:#{company.email}"
     if !apply_url.empty?
-      if apply_url.match('^http://|https://')
+      if is_complete_url
         url = apply_url
       else
         url = "http://#{apply_url}"
       end
     end
     url
+  end
+
+  def is_complete_url
+    apply_url.match("^http://|https://")
   end
 end
